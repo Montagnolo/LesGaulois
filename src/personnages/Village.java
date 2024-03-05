23ece36a4767792dcd6520f73a433c9303ced08a
Alexandre Renaudon <rnl4250a@ensfsi.onmicrosoft.com>
2024-03-05 17:45:31 +0100

    tp2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,19 +3,34 @@ package personnages;
 public class Village { 
 	private String nom;
 	private Chef chef;
-	private Gaulois[] villagois;
-	private int nbVillagois;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
 	
 	
-	public Village(String nom) { 
-		this.nom = nom; 
+	public Village(String nom,int nbVillageoisMaximum) { 
+		this.nom = nom;  
+		this.villageois = new Gaulois[nbVillageoisMaximum];
 	}
 	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois ++;
+	}
+	
+	public void trouverHabitant(int numeroVillageois) {
+	}	
+		
 	public void setChef(Chef chef) {
 		this.chef = chef;
 	}
 	
 	public String getNom() { 
 		return nom; 
+	}
+	
+	public static void main(String[] args) { 
+		Village village;
+		village = new Village("Village des Irréductibles",30);
+		Gaulois gaulois = village.trouverHabitant(30);
 	}
 }
